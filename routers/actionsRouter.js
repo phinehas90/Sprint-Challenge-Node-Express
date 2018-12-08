3a40f3dbf3a98d5f9485e2620fccc0b226ebd7d5
Phinehas <kohler.nai@gmail.com>
2018-12-08 12:43:24 -0700

    all endpoints for actions working

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -8,7 +8,28 @@ const projects = require('../data/helpers/projectModel');
 //---------Create------------------
 
 router.post('/', (req, res) => {
-    actions.insert()
+    const action = req.body;
+    projects.get(action.project_id)
+        .then(proj => {            
+            if(action.description.length < 129) {
+                if(action.description && action.notes){
+                    actions.insert(action)
+                        .then(newAction => {
+                            res.json({message: "Action was successfully added!"})
+                        })
+                        .catch(err => {
+                            res.json({message: "Could not add the action."})
+                        })
+                } else {
+                    res.json({message: "Please make sure your action has an appropriate description or notes"})
+                } 
+            } else {
+                res.json({message: "Please make sure your action description is less than 128 characters."})
+            }     
+        })
+        .catch(err => {
+            res.json({message: "The project you're trying to add actions to does not exist."})
+        })
 })
 
 //---------Read------------------
@@ -38,8 +59,43 @@ router.get('/:id', (req, res) => {
 
 //---------Update------------------
 
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const action = req.body;
+    if(action.description && action.notes){
+        if(action.description.length < 129) {
+            actions.update(id, action)
+                .then(upAct => {
+                    res.json({message: "Action successfully updated!"})
+                })
+                .catch(err => {
+                    res.status(404)
+                        .json({message: "The action with the specified ID does not exist"})
+                })
+        } else {
+            res.json({message: "Description length must be less than 128 characters."})
+        }
+    } else {
+        res.json({message: "Please enter an accurate description and notes to update the action"})
+    }
+})
+
 //---------Destroy------------------
 
-
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    actions.remove(id)
+        .then(action => {
+            if(action) {
+                res.json({message: "Action successfully deleted"})
+            } else {
+                res.json({message: "The action with the specified id does not exist"})
+            } 
+        })
+        .catch( err => {
+            res.status(500)
+                .json({message: "The action could not be removed."})
+        })
+})
 
 module.exports = router;
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -2,21 +2,30 @@ const express = require('express');
 
 const router = express.Router();
 
+
 const projects = require('../data/helpers/projectModel');
-const actions = require('../data/helpers/actionModel');
+
+
+
 
 //---------Create------------------
 
 router.post('/', (req, res) => {
     const proj = req.body;
     if(proj.name && proj.description){
-        projects.insert(proj)
+        if(proj.name.length < 129) {
+            projects.insert(proj)
             .then(projInfo => {
                 res.json({projInfo});
             })
             .catch( err => {
                 res.json({message: "Failed to add project"})
             })
+        } else {
+            res
+                .status(400)
+                .json({message: "Project name can be no longer than 128 characters"})
+        }
     } else {
         res
             .status(400)
@@ -38,6 +47,19 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    projects.get(id)
+        .then(proj => {
+            res.json(proj)
+        })
+        .catch(err => {
+            res.status(500)
+                .json({message: "The projects information could not be retrieved."})
+        })
+})
+
+
+router.get('/:id/actions', (req, res) => {
     const { id } = req.params;
     projects.getProjectActions(id)
         .then(proj => {            
@@ -61,7 +83,8 @@ router.put('/:id', (req, res) => {
     const { id } = req.params;
     const proj = req.body;
     if(proj.name && proj.description){
-        projects.update(id, proj)
+        if(proj.name.length < 129){
+            projects.update(id, proj)
             .then(upProj => {
                 res.json(upProj);
             })
@@ -69,6 +92,11 @@ router.put('/:id', (req, res) => {
                 res.status(404)
                     .json({message: "The project could not be updated"})
             })
+        } else {
+            res
+                .status(400)
+                .json({message: "Project name is too long"})
+        }
     } else {
         res
             .status(400)
